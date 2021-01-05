fadeebcdbcad84ee90bf1b71067ed72236136919
Simon <simon.hua@lionco.com>
2021-01-06 10:03:35 +1100

    Bug fixes, settings, tooltips

--- src/settings.ts
+++ src/settings.ts
@@ -3,14 +3,14 @@ import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
 /**
  * Settings as displayed in the preferences pane and used to configure the chart
  */
-export class SettingsParser extends dataViewObjectsParser.DataViewObjectsParser {
-    public chartSettings: ChartSettings = new ChartSettings();
+export class Settings extends dataViewObjectsParser.DataViewObjectsParser {
+    public invertColours: InvertColours = new InvertColours();
     public axisScaling: AxisScaling = new AxisScaling();
     public tickFormat: TickFormat = new TickFormat();
 }
 
-export class ChartSettings {
-    public toggle: boolean = false;
+export class InvertColours {
+    public show: boolean = false;
 }
 
 export class AxisScaling {
@@ -20,6 +20,8 @@ export class AxisScaling {
 }
 
 export class TickFormat {
+    public show: boolean = false;
     public x: string = "";
     public y: string = "";
+    public bucketIsDate: boolean = false;
 }
--- src/visual.ts
+++ src/visual.ts
@@ -26,9 +26,13 @@
 "use strict";
 
 import "core-js/stable";
+import "regenerator-runtime/runtime";
 import * as d3 from "d3";
 import powerbi from "powerbi-visuals-api";
 import "./../style/visual.less";
+import { Settings } from "./settings";
+import { createTooltipServiceWrapper, TooltipEventArgs, ITooltipServiceWrapper, TooltipEnabledDataPoint } from "powerbi-visuals-utils-tooltiputils";
+import VisualTooltipDataItem = powerbi.extensibility.VisualTooltipDataItem;
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import ISelectionManager = powerbi.extensibility.ISelectionManager;
@@ -40,9 +44,8 @@ import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
 import IVisualHost = powerbi.extensibility.visual.IVisualHost;
 type Selection<T extends d3.BaseType> = d3.Selection<T, any, any, any>;
+import VisualUpdateType = powerbi.VisualUpdateType;
 
-import { SettingsParser } from "./settings";
-import { valueFormatter } from "powerbi-visuals-utils-formattingutils";
 
 export class Visual implements IVisual {
     // PBI interactions
@@ -69,24 +72,25 @@ export class Visual implements IVisual {
     private data: DataPoint[];
     private dataMeasureMax: number;
     private dataMeasureMin: number;
-    private dataBucketFormat: string;
-    private dataMeasureFormat: string;
 
-    private settingsParser: SettingsParser = new SettingsParser();
+    private settings: Settings = new Settings();
+    private tooltipServiceWrapper: ITooltipServiceWrapper;
 
     constructor(options: VisualConstructorOptions) {
         this.host = options.host;
+        this.tooltipServiceWrapper = createTooltipServiceWrapper(this.host.tooltipService, options.element);
         this.selectionManager = this.host.createSelectionManager();
         this.svg = d3.select(options.element)
             .append("svg");
         this.container = this.svg.append("g");
+        
     }
 
     /**
      * Called by PowerBi to handle chart properties
      */
     public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstanceEnumeration {
-        return SettingsParser.enumerateObjectInstances(this.settingsParser, options);
+        return Settings.enumerateObjectInstances(this.settings, options);
     }
 
     /**
@@ -94,41 +98,74 @@ export class Visual implements IVisual {
      * @param options 
      */
     public update(options: VisualUpdateOptions) {
-        // Refetch data
-        this.fetchData(options);
+        this.updateSettings(options);
+        switch (options.type) {
+            case VisualUpdateType.Data:
+                this.fetchData(options);
+                break;
+            case VisualUpdateType.Resize:
+                this.resize(options);
+                break;
+            case VisualUpdateType.All:
+                this.updateSettings(options);
+                this.fetchData(options);
+                this.resize(options);
 
-        // Update settings
-        this.settingsParser = SettingsParser.parse<SettingsParser>(options.dataViews[0]);
+                // Bind deselect click event
+                this.svg.on("click", () => {
+                    if (event.defaultPrevented) return;
 
-        // Resize the container and regenerate the axes
-        this.resize(options);
+                    this.selectionManager.clear();
+                    this.redraw(null);
+                })
+
+                break;
+        }
+
+        this.regenerateAxes();
 
         // Update the bars
         // Binds data
         let bars = this.container.selectAll("rect")
             .data(this.data);
 
+        let _this = this;
+
         // Configure the generation of bars
         bars.enter()
             .append("rect")
-                .attr("x", d => { return this.x(String((<DataPoint> d).bucket)) })
+                .attr("x", (d) => { return this.x(String((<DataPoint> d).bucket)) })
                 .attr("width", this.x.bandwidth())
-                .attr("y", d => { return this.y((<DataPoint> d).line) })
+                .attr("y", (d) => { return this.y((<DataPoint> d).line) })
                 .attr("height", 0 )
-            .on("click", d => {
+            .on("click", (d) => {
                 // Allow selection only if the visual is rendered in a view that supports interactivity (e.g. Report)
                 if (this.host.allowInteractions) {
-                    this.selectionManager.select(d.selectionId);
-                    this.redraw(options);
+                    this.selectionManager.select(d.selectionId)
+                    this.redraw(null);
+                    event.preventDefault();
                 }
             });
         // Remove entries when no longer needed
         bars.exit()
             .remove();
 
+        this.renderTooltip();
         this.redraw(options);
     }
-
+    
+    /**
+     * Loads tooltips
+     * Since this gets multiple times without unloading, this could be a memory leak
+     * Dont know how it works under the hood and microsoft doesnt provide a way of freeing
+     * so its probably okay
+     */
+    private renderTooltip() {
+        this.tooltipServiceWrapper.addTooltip(
+            this.container.selectAll("rect"),
+            (tooltipEvent: DataPoint) => { return tooltipEvent.tooltipInfo; },
+            (tooltipEvent: DataPoint) => { return tooltipEvent.selectionId; })
+    }
 
     /**
      * Refreshes data store
@@ -149,27 +186,44 @@ export class Visual implements IVisual {
 
         // Map the data and generate selectionIds
         let temp = dataSource.categories[0].values.map((e, i) => {
-            return {
-                "bucket": e,
-                "measure": <number> dataSource.values[measureIndex].values[i],
-                "line": <number> dataSource.values[lineIndex].values[i],
-                "selectionId": this.host.createSelectionIdBuilder()
+            let bucket = (this.settings.tickFormat.show && this.settings.tickFormat.bucketIsDate) ? Date.parse(<string> e) : e;
+            let measure = <number> dataSource.values[measureIndex].values[i];
+            let line = <number> dataSource.values[lineIndex].values[i];
+            let selectionId = this.host.createSelectionIdBuilder()
                     .withCategory(dataSource.categories[0], i)
-                    .createSelectionId()
+                    .createSelectionId();
+            return {
+                bucket: bucket,
+                measure: measure,
+                line: line,
+                selectionId: selectionId,
+                tooltipInfo: [
+                    {
+                        displayName: "Bucket",
+                        value: bucket.toString()
+                    },
+                    {
+                        displayName: "Value",
+                        value: measure.toString()
+                    },
+                    {
+                        displayName: "Target",
+                        value: line.toString()
+                    }
+                ]
             }
         });
 
         this.dataMeasureMax = <number> dataSource.values[measureIndex].maxLocal;
         this.dataMeasureMin = <number> dataSource.values[measureIndex].minLocal;
-        this.dataBucketFormat = dataSource.categories[0].source.format;
-        this.dataMeasureFormat = dataSource.values[measureIndex].source.format;
-        
+
         // Sort
         this.data = temp.sort((a, b) => { return a.bucket > b.bucket ? 1 : -1 });
     }
 
     /**
      *  Resize the container and generate the axes
+     *  @param options
      */
     private resize(options: VisualUpdateOptions) {
         let width: number = options.viewport.width;
@@ -181,38 +235,64 @@ export class Visual implements IVisual {
         this.svg.attr("width", width);
         this.svg.attr("height", height);
         this.container.attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+    }
 
+    /**
+     * Regenerate the axes
+     */
+    private regenerateAxes() {
         // Regenerate the axes
         this.container.selectAll(".axis").remove();
 
-        let xFormatter = valueFormatter.create({ format: this.dataBucketFormat });
-        let yFormatter = valueFormatter.create({ format: this.dataMeasureFormat });
-
-        // X axis scaler
+        // X axis
         this.x = d3.scaleBand()
             .range([0, this.containerWidth])
             .domain(this.data.map(d => String(d.bucket)))
             .padding(0.2);
+        let xAxis = d3.axisBottom(this.x);
+        if (this.settings.tickFormat.show) {
+            // Different formatters for numbers and dates
+            if (this.settings.tickFormat.bucketIsDate) {
+                if (this.settings.tickFormat.x === "")
+                    xAxis.tickFormat(d3.timeFormat("%d/%m"));
+                else
+                    xAxis.tickFormat(d3.timeFormat(this.settings.tickFormat.x));
+            } else {
+                if (this.settings.tickFormat.x !== "")
+                    xAxis.tickFormat(d3.format(this.settings.tickFormat.x));
+            }
+        }
+
+        // Y axis
+        let upper = this.settings.axisScaling.show ? this.settings.axisScaling.upper : this.dataMeasureMax * 1.2;
+        let lower = this.settings.axisScaling.show ? this.settings.axisScaling.lower : this.dataMeasureMin * 0.8;
+        this.y = d3.scaleLinear()
+            .range([0, this.containerHeight])
+            .domain([upper, lower]);
+        let yAxis = this.settings.tickFormat.show && this.settings.tickFormat.y !== ""
+            ? d3.axisLeft(this.y).tickFormat(d3.format(this.settings.tickFormat.y))
+            : d3.axisLeft(this.y);
+
         // X axis
         this.container.append("g")
             .attr("class", "axis")
             .attr("transform", "translate(0," + this.containerHeight + ")")
-            .call(d3.axisBottom(this.x).tickFormat((x) => { return xFormatter.format(x) }))
+            .call(xAxis)
             .selectAll("text")
                 .attr("transform", "translate(-10,0)rotate(-45)")
                 .style("text-anchor", "end");
 
-        // Y axis scaler
-        let upper = this.settingsParser.axisScaling.show ? this.settingsParser.axisScaling.upper : this.dataMeasureMax * 1.2;
-        let lower = this.settingsParser.axisScaling.show ? this.settingsParser.axisScaling.lower : this.dataMeasureMin * 0.8;
-
-        this.y = d3.scaleLinear()
-            .range([0, this.containerHeight])
-            .domain([upper, lower]);
         // Y axis
         this.container.append("g")
-            .call(d3.axisLeft(this.y).tickFormat((x) => { return yFormatter.format(x) }))
+            .call(yAxis)
             .attr("class", "axis");
+    }
+
+    /**
+     * Update settings
+     */
+    private updateSettings(options: VisualUpdateOptions) {
+        this.settings = Settings.parse<Settings>(options.dataViews[0]);
     }
 
     /**
@@ -226,23 +306,33 @@ export class Visual implements IVisual {
 
         let currentlySelected = this.selectionManager.getSelectionIds();
 
-        bars.transition()
+        let transition = bars.transition()
             .duration(3000/count)
-            .delay((d, i) => { return (i*1000/count) })
-            .attr("fill", d => { return (<DataPoint> d).measure >= (<DataPoint> d).line ? this.colour.positive : this.colour.negative })
-            .attr("fill-opacity", d => { return currentlySelected.indexOf((<DataPoint> d).selectionId) > -1 || currentlySelected.length == 0 ? 1 : 0.4 })
-            .attr("x", d => { return this.x(String((<DataPoint> d).bucket)) })
-            .attr("width", this.x.bandwidth())
-            // y represents the starting point for the bar while height represents how long the bar is (positive only)
-            // As usual for d3, the starting point is from the top and the bar grows downwards
-            .attr("y", d => { return (<DataPoint> d).measure > (<DataPoint> d).line ? this.y((<DataPoint> d).measure) : this.y((<DataPoint> d).line) })
-            .attr("height", d => { return Math.abs(this.y((<DataPoint> d).line) - this.y((<DataPoint> d).measure)); });
+            .delay((d, i) => { return (i*1000/count) });
+
+        // Dont change colours if the only change is a resize
+        if (options === null || options.type === VisualUpdateType.Data || options.type === VisualUpdateType.All) {
+            // XOR above/below line classification with invert colour setting
+            transition.attr("fill", (d) => { return ((<DataPoint> d).measure >= (<DataPoint> d).line) !== this.settings.invertColours.show ? this.colour.positive : this.colour.negative })
+                .attr("fill-opacity", (d) => {
+                    return currentlySelected.some((e) => (<any> e).key === (<any>(<DataPoint> d).selectionId).key) || currentlySelected.length === 0 ? 1 : 0.4
+                })
+        }
+
+        if (options !== null)
+            transition.attr("x", (d) => { return this.x(String((<DataPoint> d).bucket)) })
+                .attr("width", this.x.bandwidth())
+                // y represents the starting point for the bar while height represents how long the bar is (positive only)
+                // As usual for d3, the starting point is from the top and the bar grows downwards
+                .attr("y", (d) => { return (<DataPoint> d).measure > (<DataPoint> d).line ? this.y((<DataPoint> d).measure) : this.y((<DataPoint> d).line) })
+                .attr("height", (d) => { return Math.abs(this.y((<DataPoint> d).line) - this.y((<DataPoint> d).measure)); });
     }
 }
 
-interface DataPoint {
+interface DataPoint extends TooltipEnabledDataPoint {
     bucket: any,
     measure: number,
     line: number,
     selectionId: ISelectionId
+    tooltipInfo: VisualTooltipDataItem[]
 }
